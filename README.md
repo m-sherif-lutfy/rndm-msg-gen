0bad7554bdbdde6cef0eb16644e6e915b075b019
m-sherif-lutfy <m.sherif.lutfy@gmail.com>
2021-02-24 18:30:35 +0200

    fifth commit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # README for Random Message Generator
 
 ## This project is written in JavaScript
+* An object containing three arrays.
+* Each array represents part of the random sentence.
+* The generate method concatenates the three parts then returns the final sentence.
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let rndmMsg = {
-    // an array of salutations
+    // an array of salutations.
     first : ["Oi", "Hello", "Heya", "Sup", "Evenin'", "Morning", "Hola", "Bonjour", "Ya hala"],
-    // an arry of characters
+    // an arry of characters.
     second : ["Madam", "Sir", "Joker", "Poppet", "Cap'n", "Sunshine", "Doll", "Smartass", "Prof"],
-    // an array of actions
+    // an array of actions.
     third : ["Care for some tea?",
             "Wanna hit some baseballs?",
             "How about something to eat?",
@@ -13,7 +13,14 @@ let rndmMsg = {
             "Why so serious?",
             "Write this down before I wipe it off!",
             "We need to hit the road, and don't come back no more."],
+    // method that generates the random message.        
     generate () {
+        const x = this.first[Math.floor(Math.random() * this.first.length)];
+        const y = this.second[Math.floor(Math.random() * this.second.length)];
+        const z = this.third[Math.floor(Math.random() * this.third.length)];
 
+        return `${x}, ${y}. ${z}`;
     }
 };
+
+console.log(rndmMsg.generate());
