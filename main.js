7f539d8d284e1ee797c37186c3a125699b4e53e6
m-sherif-lutfy <m.sherif.lutfy@gmail.com>
2021-02-24 17:19:57 +0200

    fourth commit

--- main.js
+++ main.js
@@ -2,9 +2,17 @@ let rndmMsg = {
     // an array of salutations
     first : ["Oi", "Hello", "Heya", "Sup", "Evenin'", "Morning", "Hola", "Bonjour", "Ya hala"],
     // an arry of characters
-    second : [],
+    second : ["Madam", "Sir", "Joker", "Poppet", "Cap'n", "Sunshine", "Doll", "Smartass", "Prof"],
     // an array of actions
-    third : [],
+    third : ["Care for some tea?",
+            "Wanna hit some baseballs?",
+            "How about something to eat?",
+            "Come join me for some booze!",
+            "Hoist the sails and head for the horizon!",
+            "You need to get some rest before we go.",
+            "Why so serious?",
+            "Write this down before I wipe it off!",
+            "We need to hit the road, and don't come back no more."],
     generate () {
 
     }
